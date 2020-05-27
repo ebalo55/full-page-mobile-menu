414e531f7c0b372c86ad3ed74446c90240213384
ebalo <tuema55@gmail.com>
2020-05-28 00:10:35 +0200

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <div class="menu-toggle" @click="isMenuOpen = true">
+            <span class="fas fa-bars"></span>
+        </div>
+        <img class="background-img" src="/assets/background.jpg">
+        <div :class="menu_class" :style="menu_transition">
+            <div class="menu-toggle-internal" @click="isMenuOpen = false"
+                :style="toggle_internal_transition">
+                <span class="fas fa-times"></span>
+            </div>
+            <ul>
+                <li v-for="(item, index) of urls" :key="index">
+                    <a :href="item.href" v-html="item.label"></a>
+                    <hr>
+                </li>
+            </ul>
+            <ul class="socials">
+                <li v-for="(item, index) of socials" :key="index">
+                    <a :href="item.href" v-html="item.label"></a>
+                </li>
+            </ul>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    data: function() {
+        return {
+            isMenuOpen: false,
+            urls: [
+                {href: "#", label: `<span class="fas fa-home"></span> Home`},
+                {href: "#", label: `<span class="fas fa-box"></span> Orders`},
+                {href: "#", label: `<span class="fas fa-truck"></span> Delivery`},
+                {href: "#", label: `<span class="fas fa-user-shield"></span> Privacy`},
+                {href: "#", label: `<span class="fas fa-cog"></span> Settings`},
+                {href: "#", label: `<span class="far fa-question-circle"></span> FAQ`},
+            ],
+            socials: [
+                {href: "#", label: `<span class="fab fa-facebook"></span>`},
+                {href: "#", label: `<span class="fab fa-twitter"></span>`},
+                {href: "#", label: `<span class="fab fa-instagram"></span>`},
+                {href: "#", label: `<span class="fab fa-linkedin"></span>`},
+            ]
+        };
+    },
+    computed: {
+        menu_class: function() {
+            return `menu-wrapper ${this.isMenuOpen ? "show" : ""}`;
+        },
+        menu_transition: function() {
+            return !this.isMenuOpen ? "transition-delay: .5s;" : "";
+        },
+        toggle_internal_transition: function() {
+            return this.isMenuOpen ? "transition-delay: .5s;" : "";
+        }
+    }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
